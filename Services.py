a0d39342acad2f8dacdd39ffe5d984625910c57c
bitan-mondal <bitanmondal011@gmail.com>
2020-04-19 21:44:27 +0530

    Updated pyDocs

--- Services.py
+++ Services.py
@@ -3,19 +3,43 @@ import random
 import mysql.connector
 import pyfiglet
 
-"""It generates Item_ID through random function whenever we need to insert a new row in the Database"""
 
 def genID():
+    """ 
+    Descripton: 
+        This method generates Item_ID through random function whenever we need to insert a new row in the Database
+    Input Parameters:
+        NA
+    Output: 
+        Item ID
+    """
+
     lettersAndDigits = string.ascii_lowercase + string.digits
     return ''.join((random.choice(lettersAndDigits) for i in range(10)))
 
-"""It establishes Database connecting using the User Name and Password"""
 
 def get_db_connection():
+    """ 
+    Descripton: 
+        This method establishes Database connection using the User Name and Password
+    Input Parameters:
+        NA
+    Output:
+        Returns touple of Database connections and its curser
+    """
+
     db = mysql.connector.connect(host="localhost",user="Bitan",passwd='Bitan987',database="test")
     return db, db.cursor()
 
-"""It is used to Format Text"""
 
 def decorative_text(word):
+    """ 
+    Descripton: 
+        This method is used to is used to Format Text
+    Input Parameters:
+        Takes input as normal text
+    Output: 
+        Returns formated text
+    """
+
     print(pyfiglet.figlet_format(word))
